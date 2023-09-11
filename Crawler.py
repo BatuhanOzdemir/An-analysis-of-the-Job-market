6d8329c3eecee4e121fe2594149ed287e6290a01
BatuhanOzdemir <batuhan.ozdemir@yandex.com>
2023-09-11 15:46:06 +0300

    Add files via upload

---
+++ Crawler.py
@@ -0,0 +1,74 @@
+import logging
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s:%(message)s',
+     level=logging.INFO)
+
+
+class Crawler:
+    def __init__(self, urls=[]):
+        self.urls_for_job_ads = []
+        self.visited_urls = []
+        self.urls_to_visit = urls
+
+
+    def download_url(self, url):
+        if url is not None:
+            return requests.get(url).text
+
+    def get_linked_urls(self, url, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        pony = soup.findAll('a')
+        for link in pony:
+            path = link.get("href")
+            if path and path.startswith('/'):
+                path = urljoin(url, path)
+            yield path
+
+    def add_url_to_visit(self, url):
+        if url not in self.urls_to_visit and url not in self.visited_urls and self.sanitizeToVisitList(url):
+            if url.__contains__("is-ilani")and any(a.isdigit() for a in url):
+                self.urls_for_job_ads.append(url)
+
+            self.urls_to_visit.append(url)
+
+
+    def sanitizeToVisitList(self,url):
+        exclusionList = ["apple","google","cimri","instagram","twitter","facebook","ik-blog",
+                            "tercih","pozisyonlar","universite","kariyer-rehberi","bolumler",
+                            "kariyer-kampuste","kariyer-gunleri","tercih-motoru",]
+        for unwantedURL in exclusionList:
+            if url is None or url.__contains__(unwantedURL):
+                return False
+        return True
+
+
+    def crawl(self, url):
+        html = self.download_url(url)
+        for url in self.get_linked_urls(url, html):
+            self.add_url_to_visit(url)
+
+
+
+
+
+    def run(self):
+        while self.urls_to_visit and len(self.urls_for_job_ads) <= 600:
+            url = self.urls_to_visit.pop(0)
+            with open("file.txt","w") as output:
+                output.write(str(self.urls_for_job_ads)+"\n")
+            logging.info(f'Crawling:{url}')
+            try:
+                self.crawl(url)
+            except:
+                logging.exception(f'Failed to Crawl:{url}')
+            finally:
+                self.visited_urls.append(url)
+    def print_url_ads(self):
+        print(self.urls_for_job_ads)
+        print(len(self.urls_for_job_ads))
+    def get_urls_for_job_ads(self):
+        return self.urls_for_job_ads
---
+++ extractAdInfo.py
@@ -0,0 +1,43 @@
+from Crawler import Crawler
+from bs4 import BeautifulSoup
+import requests
+from JobAd import JobAd
+import csv
+import time
+
+crawl = Crawler(urls=['https://kariyer.net'])
+start = time.time()
+crawl.run()
+url_list = crawl.get_urls_for_job_ads()
+
+
+def gather_ads():
+    ads_list = []
+    for url in url_list:
+        try:
+            html = requests.get(url).text
+            soup = BeautifulSoup(html, 'html.parser')
+            ad_info = soup.findAll(name="script", attrs={"type":"text/javascript","data-n-head":"ssr"})
+            ad_update_date = soup.find("div", {"class": "updated-date"}).getText()
+            ad_content = soup.find("div", {"class": "job-detail-content"}).getText()
+            ad_obj = JobAd(ad_info,ad_update_date, ad_content)
+            ads_list.append(ad_obj)
+        except AttributeError:
+            continue
+    return ads_list
+
+
+listOfAds = gather_ads()
+
+#creating a csv file
+fields = ["Çalışma Şekli","Sektör","Firma","İlani Veren Firma","Pozisyon","İlan-ID","Lokasyon","İlan-Statüsü", "Detail-Aday", "İlan-Metni","Son-Güncelleme"]
+
+with open("Kariyer.csv","w",newline='') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(fields)
+
+    for ad in listOfAds:
+        csv_writer.writerows([ad.get_info()])
+
+end = time.time()
+print("It took",end - start)
